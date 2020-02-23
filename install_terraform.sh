6d490f589ab2904bb2f49dc91fa6c5e2f3c10334
EC2 Default User <ec2-user@ip-172-31-10-92.eu-west-1.compute.internal>
2020-02-23 14:37:18 +0000

    script terraform

---
+++ install_terraform.sh
@@ -0,0 +1,3 @@
+wget https://releases.hashicorp.com/terraform/0.12.21/terraform_0.12.21_linux_amd64.zip
+sudo unzip terraform_0.12.21_linux_amd64.zip -d /usr/bin
+rm -rf terraform_0.12.21_linux_amd64.zip 
